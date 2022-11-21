616ea4bd601d750d4e0ffdb9c6a84ad96a21126d
roshmaep <roshmaep1999@gmail.com>
2022-11-21 12:59:36 +0530

    prime

--- src/Main.java
+++ src/Main.java
@@ -28,6 +28,25 @@ public class Main {
                     smallest = z < temp ? z : temp;
                     System.out.println("largest=" + smallest);
                     break;
+                case 3:
+                    System.out.println("Enter a number");
+                    x = sc.nextInt();
+                    boolean flag = false;
+                    for (int i = 2; i <= x / 2; ++i) {
+                        // condition for nonprime number
+                        if (x % i == 0) {
+                            flag = true;
+                            break;
+                        }
+                    }
+
+                    if (!flag)
+                        System.out.println(x + " is a prime number.");
+                    else
+                        System.out.println(x + " is not a prime number.");
+
+                    break;
+
             }
         } while(c!=6);
     }
